d7b4f1d17ad00df5a6b93eb74a11cb851967c4e9
Sri Jai Ganapathy S N <jainthpadmasini80@gmail.com>
2024-12-15 18:04:45 +0530

    homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fab/firebase_options.dart';
+import 'package:fab/screens/homepage.dart';
 import 'package:fab/screens/routinelistscreen.dart';
 import 'package:fab/screens/taskreveal.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,10 +19,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        builder: (context, child) => MediaQuery(
-            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-            child: child!),
-        debugShowCheckedModeBanner: false,
-        home: Routinelistscreen());
+      builder: (context, child) => MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
